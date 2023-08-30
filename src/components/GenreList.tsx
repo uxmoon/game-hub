a4837097e048ba3066e74b066bf71607d45d2c07
uxmoon <uxmoon@gmail.com>
2023-08-30 15:55:22 -0300

    Highlight selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -11,9 +11,10 @@ import getCroppedImageUrl from '../services/image-url'
 
 interface Props {
   onSelectGenre: (genre: Genre) => void
+  selectedGenre: Genre | null
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres()
 
   if (error) return null
@@ -34,6 +35,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               fontSize='lg'
               variant='link'
               onClick={() => onSelectGenre(genre)}
+              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
             >
               {genre.name}
             </Button>
